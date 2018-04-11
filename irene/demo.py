81cbef3b2b8ede5107f5273775c20a51ca958343
Marc  Matvienko <marcmatvienko@marcmac.local>
2018-04-11 07:29:23 -0500

    hard coded search for NN,VBD

--- irene/demo.py
+++ irene/demo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import spacy
+from spacy.matcher import Matcher
 import en_core_web_sm
 from spacy import displacy
 nlp = en_core_web_sm.load()
@@ -13,10 +14,23 @@ tags["NN"] = "noun"
 tags["VBD"] = "verb of past tense"
 tags["IN"] =  "preposition"
 
-doc = nlp(u'the quick brown fox jumped over the lazy dog')
+doc = nlp(u'the quick brown fox, jumped over the lazy dog')
+
+matcher = Matcher(nlp.vocab)
+pattern = [{'TAG': 'NN'}, {'IS_PUNCT': True}, {'TAG': 'VBD'}]
+matcher.add('query1', None, pattern)
+
+matches = matcher(doc)
+
+for match_id, start, end in matches:
+    string_id = nlp.vocab.strings[match_id]  # 'HelloWorld'
+    span = doc[start:end]                    # the matched span
+    print(string_id)
+    print(span)
 
 for token in doc:
-    print("'",token.text,"' is a ", tags[token.tag_])
+    print("'",token.text,"' is a ", token.tag_)
+
 
 print("would you like to see a dank chart? (y/n)")
 choice = str(input())
