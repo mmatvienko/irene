9c56d441000d454df51b04ea5733aebf72d1b2be
Marc  Matvienko <marcmatvienko@marcmac.local>
2018-04-10 21:51:47 -0500

    removed demo code

--- irene/parser.py
+++ irene/parser.py
@@ -1,17 +1,6 @@
 #!/usr/bin/env python3
 
 import spacy
-import en_core_web_sm
-from spacy import displacy
-nlp = en_core_web_sm.load()
-
-doc = nlp(u'the quick brown fox jumped over the lazy dog')
-
-for token in doc:
-    print(token.text," is a ", token.tag_, " and depends on " , token.dep_)
-
-# display at localhost:5000
-displacy.serve(doc, style='dep')
 
 # have core_parser (takes raw text)
 # JSON, CSV, etc will all be subclasses of core_parser
@@ -20,4 +9,3 @@ displacy.serve(doc, style='dep')
 class Core_Parser():
 	def __init__(self):
 		pass
-	
