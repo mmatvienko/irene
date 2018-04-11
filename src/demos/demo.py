2fbb001d2e468698cc90ab9cd3cdf19b55ce6d62
Marc  Matvienko <marcmatvienko@wireless-10-147-14-223.public.utexas.edu>
2018-04-11 11:24:48 -0500

    i dont even know

---
+++ src/demos/demo.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import spacy
+from spacy import displacy
+from spacy.matcher import Matcher
+import en_core_web_sm
+import json
+nlp = en_core_web_sm.load()
+
+# tags from https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html
+tags = {}
+tags["DT"] = "determiner"
+tags["JJ"] = "adjective"
+tags["NN"] = "noun"
+tags["VBD"] = "verb of past tense"
+tags["IN"] =  "preposition"
+
+# read in data
+data = json.load(open('../data/sample.json'))
+
+# extract all text
+docs = []
+for complaint in data["Results"]:
+	docs.append(nlp(str(complaint["Summary"]).lower()))
+
+matcher = Matcher(nlp.vocab)
+pattern = [{'TAG': 'DT'},{'TAG': 'JJ'},{'TAG': 'NN'}]
+# change pattern to have specfic
+matcher.add('pronoun verb', None, pattern)
+
+matches = []
+for doc in docs:
+	matches.append(matcher(doc))
+
+for match in matches:
+	for match_id, start, end in match:
+	    string_id = nlp.vocab.strings[match_id]  # 'HelloWorld'
+	    span = doc[start:end]                    # the matched span
+	    print(span)
+
+# for token in doc:
+#    print("'",token.text,"' is a ", token.tag_)
+
+fox = nlp(u'the quick brown fox, jumped over the lazy dog')
+
+print("would you like to see a dank dep. chart? (y/n)")
+choice = str(input())
+if choice == "y":
+	# display at localhost:5000
+	displacy.serve(fox, style='dep')
+else:
+	print("sucks to suck")
