cf7cc98373910c10f061b85473e0c0d2c7969044
Marc Matvienko <m.matvienko@utexas.edu>
2018-04-30 21:06:53 -0500

    formatting/InputHandler fix

--- src/query_engine.py
+++ src/query_engine.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import spacy
-from parser import *
+
+from parser import JSON_Parser
 from collection import Collection
-from input import *
+from input import InputHandler
+
 
 class Query_Engine:
     def __init__(self, file='../data/sample.json'):
@@ -11,15 +13,14 @@ class Query_Engine:
         self.file = file
         parser = JSON_Parser(file)
         self.collection = parser.get_collection()
-		handler = input.Input_Handler()
-		
+        self.handler = InputHandler()
+
     def search_corp(self, str_query):
         # probably best if a generator is returned
         self.last_query = str_query
-		processed_query = handler.process_query(str_query)
+        processed_query = self.handler.process_query(str_query)
         span = self.collection.match(processed_query)
         # how will we handle different file typers
-        print(" successful query call for ", query)
+        print("successful query call for ", str_query)
         print(span)
         return "(use the query on the parser)"
-	
