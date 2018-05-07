d0cc45314d244be4f36ab0413d9932aaf001c229
Krishna Chittur <kchittur@cs.utexas.edu>
2018-05-07 13:20:48 -0500

    Updated regex demo

--- src/demos/regex_demo_basic.py
+++ src/demos/regex_demo_basic.py
@@ -7,12 +7,16 @@ import en_core_web_sm
 import re
 nlp = en_core_web_sm.load()
 
-doc = nlp('The US president is also know as the United States president these days.')
+doc = nlp(u'The US president is also know as the United States president these days.')
+
+us_regex = r'[Uu](nited)? ?[Ss](tates)?'
+us_flag = lambda text: bool(re.compile(us_regex).match(text))
+is_us = nlp.vocab.add_flag(us_flag)
 
 matcher = Matcher(nlp.vocab)
 us_president_pattern = [
-    {'REGEX': '^([Uu](\\.?|nited) ?[Ss](\\.?|tates)'},
-    {'LOWER': 'president'}
+    {is_us: True},
+    {'LOWER': 'president', 'TAG': 'NN'}
 ]
 
 matcher.add('regex test', None, us_president_pattern)
@@ -21,10 +25,10 @@ print('Printing matches:')
 match = matcher(doc)
 for match_id, start, end in match:
 	span = doc[start:end]
-	print(span)
-	print (start)
-	print(end)
+	print("Found \"{}\" at range ({},{})".format(span,start,end))
 
+# Note that multiple words cannot be matched
+    
 print("Display? (y/n)")
 choice = str(input())
 if choice == "y":
